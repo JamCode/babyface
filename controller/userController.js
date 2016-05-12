370dff086869759f5ba21ee88b69b748b09c7c25
wanghan <wh85125@163.com>
2016-05-12 20:45:48 +0800

    #

--- controller/userController.js
+++ controller/userController.js
@@ -1,1 +1,22 @@
 var userModel = require('../model/userModel.js');
+
+var userController = {};
+
+userController.get = function(req, res){
+    res.send('user test');
+}
+
+userController.post = function(req, res){
+
+}
+
+userController.put = function(req, res){
+
+}
+
+
+userController.delete = function(req, res){
+
+}
+
+module.exports = userController;
