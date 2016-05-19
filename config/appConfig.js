65f59e4a69b31780f917141a368087ed4e85bb01
wanghan <wh85125@163.com>
2016-05-19 17:17:20 +0800

    一些注释

--- config/appConfig.js
+++ config/appConfig.js
@@ -14,13 +14,15 @@ exports.redisConnection = {
     auth_pass: process.env.redis_auth_pass
 };
 
+
+//redis中hash表配置
 exports.redisHashTable = {
     userAccountHash: 'userAccountHash',
     userTokenHash :'userTokenHash',
     quoteHash: 'quoteHash'
 };
 
-//api 认证加密密钥
+//如果需要token校验，api 认证加密密钥
 exports.secretKey = {
     secret: 'babyface'
 };
@@ -30,6 +32,7 @@ exports.logConfig = {
     level: 'DEBUG'
 };
 
+//单元测试连接的环境
 exports.unitTestConfig = {
     hostname: '127.0.0.1',
     port: 3001
