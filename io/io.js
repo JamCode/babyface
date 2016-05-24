478ec2246abd4b50356bd924df36740ef320d996
wanghan <wh85125@163.com>
2016-05-24 10:38:29 +0800

    #

--- io/io.js
+++ io/io.js
@@ -1,7 +1,50 @@
+var authe = require('../utility/authe.js');
+var log = require('../utility/log.js');
+var constant = require('../utility/constant.js');
+
+//长连接
 
 exports.connectionEntry = function(socket){
+    //长连接定义入口
+    //connection 事件触发
     console.log('connection');
-    setInterval(function(){
-        socket.emit('data', { msg:'hello babyface long connection' });
-    }, 2000);
+    socket.emit('authorize', {});//请求token认证
+    socket.on('authorize', function(data){
+        //验证token
+        authe.autheToken(data.token, function(err, code){
+            if (err) {
+                log.error(err, log.getFileNameAndLineNum(__filename));
+
+                socket.emit('authorized', {
+                    code: constant.returnCode.ERROR,
+                    msg: err
+                });
+            }else{
+                if (code === constant.returnCode.TOKEN_INVALID) {
+                    log.debug('token invalid');
+                    socket.emit('authorized', {
+                        code: constant.returnCode.TOKEN_INVALID,
+                        msg: 'token invalid'
+                    });
+                }
+                if (code === constant.returnCode.TOKEN_VALID) {
+                    log.debug('token valid');
+                    socket.emit('authorized', {
+                        code: constant.returnCode.TOKEN_VALID,
+                        msg: 'token valid'
+                    });
+
+                    //定义各类事件响应
+                    ioHandle(socket);
+                }
+            }
+        });
+    });
+}
+
+
+function ioHandle(socket){
+    socket.on('data', function(data){
+        socket.emit('data', data);
+    });
 }
