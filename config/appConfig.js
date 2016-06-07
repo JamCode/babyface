ac5f765318b7ab40083721f4edbcb32ff9ca1d74
wanghan <wh85125@163.com>
2016-06-07 17:23:56 +0800

    #

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,13 +2,13 @@ var path = require('path');
 
 exports.port = 3001;
 
-exports.mysqlConnection = {
-    host: process.env.dbhost,
-    user: process.env.dbuser,
-    password: process.env.dbpassword,
-    database: process.env.database,
-    port: process.env.dbport
-};
+// exports.mysqlConnection = {
+//     // host: process.env.dbhost,
+//     // user: process.env.dbuser,
+//     // password: process.env.dbpassword,
+//     // database: process.env.database,
+//     // port: process.env.dbport
+// };
 
 exports.redisConnection = {
     auth_pass: process.env.redis_auth_pass
