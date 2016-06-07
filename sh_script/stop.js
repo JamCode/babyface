754f4207a34233aae53e2a6b076035fd606ef4a1
wanghan <wh85125@163.com>
2016-06-07 14:41:51 +0800

    #add start stop

--- sh_script/stop.js
+++ sh_script/stop.js
@@ -2,4 +2,4 @@ var path = require('path');
 require('shelljs/global');
 console.log(__dirname);
 var mainPath = path.dirname(__dirname);
-exec('pm2 start '+path.join(mainPath, 'app.js'));
+exec('pm2 stop '+path.join(mainPath, 'app.js'));
